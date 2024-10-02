1241de7f8a0044f9be05e285bb5f06b8c1ca45b5
Felipe Bernardo de Oliveira <felipebernardo55@hotmail.com>
2024-10-02 14:58:32 -0300

    wip

--- src/app/components/contato/contato.component.ts
+++ src/app/components/contato/contato.component.ts
@@ -15,12 +15,20 @@ export class ContatoComponent implements OnInit {
   contato: Array<any> = [
     {
       titulo: 'Whatsapp',
-      link: ''
+      link: 'https://wa.me/55981500256',
+      image: '../../../assets/whatsapp.png',
+      class: 'img-whatsapp botao-contato'
     },
     {
       titulo: 'Ifood',
-      link: ''
+      link: 'https://www.ifood.com.br/delivery/sao-jose-do-rio-preto-sp/delicias-da-cheiloca-distrito-industrial-waldemar-de-oliveira-verdi/de93e0d1-7075-4f19-98bc-3180d0ff9b36?utm_medium=share',
+      image: '../../../assets/ifood.png',
+      class: 'img-ifood botao-contato'
     }
   ]
+
+  goTo(link: string): void {
+    window.open(link, '_blank');
+  }
 
 }
